cb092c0d04a36bb6ea8565adb2a76131f1ad86f2
Nick Stankovich <Stankovich.Nicholas09@bloomfield.org>
2019-01-22 17:41:07 -0500

    Adding Robot.java back in

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import edu.wpi.first.wpilibj.RobotBase;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+public class Robot extends TimedRobot {
+
+  Joystick stickDrive = new Joystick(0);
+  HotSticks operator;
+  
+
+  @Override
+  public void robotInit() {
+    operator = new HotSticks(0);
+
+  }
+
+  @Override
+  public void autonomousInit() {
+   
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
