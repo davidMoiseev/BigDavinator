68eeb29aa6f315f33e28d005ca33a5e1ad27cfad
olechiw <jakob.olechiw@gmail.com>
2019-01-25 16:46:34 -0500

    Changing HotLog to multithreaded system

--- src/main/java/frc/robot/HotLog.java
+++ src/main/java/frc/robot/HotLog.java
@@ -7,12 +7,13 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Add your docs here.
  */
-public class HotLog {
+public class HotLog {    
     public static final String LOGS_DIRECTORY = "/home/lvuser/logs/";
 
     public static Date LoggerStartDate = new Date();
@@ -20,8 +21,50 @@ public class HotLog {
     public static final String DELIMITER = "\t";
 
     private static Map<String, String> logMap = new LinkedHashMap<>();
+    private static String output = "";
+    private static Object lockObject = new Object();
 
-    private static boolean changed = false;
+    private static class LoggerThread
+    {
+        private FileWriter fileWriter;
+        public void WriteToFile()
+        {
+            try {
+                long currentTime = System.currentTimeMillis();
+                StringBuilder s = new StringBuilder();
+                s.append(String.valueOf(currentTime)).append(DELIMITER);
+    
+                for (Map.Entry<String, String> entry : logMap.entrySet()) {
+                    s.append(entry.getValue()).append(DELIMITER);
+                    entry.setValue("");
+                }
+                s.append("\n");
+    
+    
+    
+                String fileName = LOGS_DIRECTORY + new SimpleDateFormat("yyyy-mm-dd hh:mm:ss").format(LoggerStartDate)
+                        + ".txt";
+    
+                File f = new File(fileName);
+                if (!f.exists()) {
+                    new File(LOGS_DIRECTORY).mkdirs();
+                    f.createNewFile();
+                    fileWriter = new FileWriter(f);
+                }
+    
+                if (fileWriter == null)
+                    fileWriter = new FileWriter(f);
+                fileWriter.append(output);
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Notifier n = new Notifier(() -> {
+        
+    });
 
     public static void LogValue(String key, Integer value) {
         LogValue(key, Double.valueOf(value));
@@ -37,50 +80,36 @@ public class HotLog {
     }
 
     public static void LogValue(String key, String value) {
-        if (logMap.keySet().contains(key))
+        synchronized (lockObject)
         {
-            logMap.put(key, value);
-            changed = true;
+            if (logMap.keySet().contains(key)) {
+                logMap.put(key, value);
+            }
         }
     }
 
     public static void Setup(String... valsToLog) {
-        logMap = new LinkedHashMap<>();
+
         StringBuilder s = new StringBuilder();
         s.append("TimeStep").append(DELIMITER);
 
-        for (int i = 0; i < valsToLog.length; ++i) {
-            logMap.put(valsToLog[i], "");
-            s.append(valsToLog[i]).append(DELIMITER);
+        synchronized (lockObject) {
+
+            logMap = new LinkedHashMap<>();
+            
+            for (int i = 0; i < valsToLog.length; ++i) {
+                logMap.put(valsToLog[i], "");
+                s.append(valsToLog[i]).append(DELIMITER);
+            }
         }
         s.append("\n");
+        output = s.toString();
         
-        String output = s.toString();
-        System.out.println(output);
-
-        try {
-            String fileName = LOGS_DIRECTORY + new SimpleDateFormat("yyyy-mm-dd hh:mm:ss").format(LoggerStartDate)
-                    + ".txt";
-
-            File f = new File(fileName);
-            if (!f.exists()) {
-                new File(LOGS_DIRECTORY).mkdirs();
-                f.createNewFile();
-            }
-
-            FileWriter writer = new FileWriter(f);
-            writer.append(output);
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        timer.start();
     }
 
     private static Timer timer = new Timer();
+
     public static void WriteToFile() {
-        if (!changed)
-            return;
         double timeStep = timer.get();
         timer.reset();
         try {
@@ -108,6 +137,5 @@ public class HotLog {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        changed = false;
     }
 }
