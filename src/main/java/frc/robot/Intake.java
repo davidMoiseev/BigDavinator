31a58342bdd4b275ec846294209a6ec29e401b61
aidan.sweeney@comcast.net <aidan.sweeney@comcast.net>
2019-01-31 16:49:05 -0500

    Finished arm code
    Signed-off-by: aidan.sweeney@comcast.net <aidan.sweeney@comcast.net>

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Intake {
+
+    Joystick joystick;
+    public static final int TALON_INTAKE_TOP = 2337;
+    public static final int TALON_INTAKE_BOTTOM = 4362;
+
+    WPI_TalonSRX TopTalon = new WPI_TalonSRX(TALON_INTAKE_TOP);
+    WPI_TalonSRX BottomTalon = new WPI_TalonSRX(TALON_INTAKE_BOTTOM);
+
+    public void intake(){
+        TopTalon.set(ControlMode.PercentOutput, 0.0);
+        BottomTalon.set(ControlMode.PercentOutput, 0.0);
+    }
+
+    public void allOff() {
+        TopTalon.set(ControlMode.PercentOutput, 0.0);
+        BottomTalon.set(ControlMode.PercentOutput, 0.0);
+    }
+    
+    public void runIntake(){
+        double speed = /*HotSticks.DriverLY()*/ joystick.getRawAxis(1);
+        /*ideally if joystick is forward, then intake spits out, if joystick is backward, intake pulls in */
+        TopTalon.set(ControlMode.PercentOutput, speed);
+        BottomTalon.set(ControlMode.PercentOutput, -speed);
+    }
+
+}
---
+++ src/main/java/org/hotteam67/HotLogger.java
@@ -0,0 +1,175 @@
+package org.hotteam67;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * Add your docs here.
+ */
+public class HotLogger {
+    public static final String LOGS_DIRECTORY = "/home/lvuser/logs/";
+
+    public static final String DELIMITER = "\t";
+    public static final String EMPTY = "";
+    public static final Double ROW_TIMEOUT_SECONDS = .02;
+    public static final Double LOG_PERIOD_SECONDS = 1.0;
+
+    private static boolean onNewRow = true;
+    private static Map<String, String> currentRow = new LinkedHashMap<String, String>();
+    private static double currentRowTime;
+
+    private static Notifier logScheduler = new Notifier(LogThread::WriteToFile);
+
+    public static void Setup(String... valsToLog) {
+
+        currentRow.clear();
+        StringBuilder headerBuilder = new StringBuilder();
+        headerBuilder.append("Time Step").append(DELIMITER);
+        for (int i = 0; i < valsToLog.length; ++i) {
+            currentRow.put(valsToLog[i], EMPTY);
+            headerBuilder.append(valsToLog[i]).append(DELIMITER);
+        }
+
+        logScheduler.stop();
+        LogQueue.RestartQueue(headerBuilder.toString());
+        logScheduler.startPeriodic(LOG_PERIOD_SECONDS);
+    }
+
+    public static void Log(String key, Integer value) {
+        Log(key, Double.valueOf(value));
+    }
+
+    public static void Log(String key, Double value) {
+        String stringValue = " ";
+        try {
+            stringValue = String.valueOf(value);
+        } catch (Exception ignored) {
+        }
+        Log(key, stringValue);
+    }
+
+    public static void Log(String key, String value) {
+        if (!currentRow.containsKey(key))
+            return;
+
+        if (onNewRow) {
+            currentRowTime = Timer.getFPGATimestamp();
+        }
+
+        if (!currentRow.get(key).equals(EMPTY)) {
+            PushCurrentRow();
+            Log(key, value);
+            return;
+        } else
+            currentRow.put(key, value);
+
+        if (Timer.getFPGATimestamp() - currentRowTime > ROW_TIMEOUT_SECONDS) {
+            PushCurrentRow();
+        }
+    }
+
+    private static void PushCurrentRow() {
+        try {
+            LinkedHashMap<String, String> newMap = new LinkedHashMap<>();
+            for (Map.Entry<String, String> currentEntry : currentRow.entrySet()) {
+                newMap.put(currentEntry.getKey(), currentEntry.getValue());
+            }
+            LogRow row = new LogRow(newMap, String.valueOf(currentRowTime));
+            LogQueue.PushToQueue(row);
+        } catch (Exception ignored) {
+        }
+        for (Map.Entry<String, String> entry : currentRow.entrySet()) {
+            entry.setValue(EMPTY);
+        }
+        onNewRow = true;
+    }
+
+    private static class LogRow {
+        public final Map<String, String> Values;
+        public final String TimeStamp;
+
+        public LogRow(Map<String, String> values, String logTimeStamp) {
+            this.Values = values;
+            this.TimeStamp = logTimeStamp;
+        }
+    }
+
+    private static class LogThread {
+        /**
+         * Function called only by logging thread
+         */
+        public static synchronized void WriteToFile() {
+            try {
+                FileWriter fileWriter = null;
+                String fileName = LOGS_DIRECTORY
+                        + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(LogQueue.GetDate()) + ".txt";
+
+                File f = new File(fileName);
+                if (!f.exists()) {
+                    new File(LOGS_DIRECTORY).mkdirs();
+                    f.createNewFile();
+
+                    fileWriter = new FileWriter(f);
+                }
+
+                if (fileWriter == null)
+                    fileWriter = new FileWriter(f);
+                fileWriter.append(LogQueue.FlushQueue());
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class LogQueue {
+        /**
+         * Log structures shared between threads
+         */
+        private static List<LogRow> logQueue = new ArrayList<>();
+        private static String headerToOutput = "";
+        private static Date logDate = new Date();
+
+        private static synchronized String FlushQueue() {
+            StringBuilder output = new StringBuilder(headerToOutput);
+            output.append("\n");
+
+            for (int i = 0; i < logQueue.size(); ++i) {
+                LogRow row = logQueue.get(i);
+                output.append(row.TimeStamp).append(DELIMITER);
+                for (Map.Entry<String, String> e : row.Values.entrySet()) {
+                    output.append(e.getValue()).append(DELIMITER);
+                }
+                if (i + 1 < logQueue.size())
+                    output.append("\n");
+            }
+            headerToOutput = "";
+            logQueue.clear();
+            return output.toString();
+        }
+
+        private static synchronized Date GetDate() {
+            return logDate;
+        }
+
+        public static synchronized void PushToQueue(LogRow row) {
+            logQueue.add(row);
+        }
+
+        public static synchronized void RestartQueue(String header) {
+            headerToOutput = header;
+            logDate = new Date();
+            logQueue.clear();
+        }
+    }
+}
