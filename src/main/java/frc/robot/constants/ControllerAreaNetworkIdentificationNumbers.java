868412d7128006117db5a0f9b190a6463023a723
olechiw <jakob.olechiw@gmail.com>
2019-02-02 09:53:35 -0500

    Added ControllerAreaNetworkIdentificationNumbers

---
+++ src/main/java/frc/robot/constants/ControllerAreaNetworkIdentificationNumbers.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.constants;
+
+/**
+ * ControllerAreaNetworkIdentificationNumbers
+ */
+public class ControllerAreaNetworkIdentificationNumbers
+{
+    public static final int RIGHT_DRIVE_1 = 1;
+    public static final int RIGHT_DRIVE_2 = 2;
+
+    public static final int LEFT_DRIVE_1 = 3;
+    public static final int LEFT_DRIVE_2 = 4;
+    
+    public static final int H_DRIVE = 7;
+
+    public static final int ELEVATOR_1 = 5;
+    public static final int ELEVATOR_2 = 6;
+
+    public static final int SHOULDER = 8;
+    public static final int WRIST = 9;
+    public static final int INTAKE = 10;
+}
