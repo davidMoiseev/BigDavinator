364595fa9987b8d3b15a06f983c3f8638c27e91c
David Moiseev <david.moiseev@gmail.com>
2019-02-24 19:07:19 -0500

    Fix formating to make the enums readable

--- src/main/java/frc/robot/constants/ManipulatorSetPoint.java
+++ src/main/java/frc/robot/constants/ManipulatorSetPoint.java
@@ -10,14 +10,26 @@ package frc.robot.constants;
 public enum ManipulatorSetPoint
 {
     // 0 16 115
-    firstPostion(3, 0, 0), carry_front(3, 0, 10), carry_back(3, 0, -10), hatch_low_front(17, 167, 90), hatch_low_back(
-            17, -167, -90), hatch_mid_front(5, 14, 90), hatch_mid_back(5, -14, -90), hatch_high_front(33, 8.5,
-                    90), hatch_high_back(33, -8.5, -90), cargo_pickup_front(2, 135, 113), cargo_pickup_back(2, -135,
-                            -113), cargo_shuttle_front(3, 29, 107), cargo_shuttle_back(3, -29,
-                                    -107), cargo_rocketLow_front(21, 160, 72), cargo_rocketLow_back(21, -160,
-                                            -72), cargo_rocketMid_front(9.5, 10, 83), cargo_rocketMid_back(9.5, -10,
-                                                    -83), cargo_rocketHigh_front(33, 1.5, 61), cargo_rocketHigh_back(33,
-                                                            -1.5, -61), manipulator_safe_minimum(36, 0, 0);
+    firstPostion(3, 0, 0),
+    carry_front(3, 0, 10), 
+    carry_back(3, 0, -10), 
+    hatch_low_front(17, 167, 90), 
+    hatch_low_back(17, -167, -90), 
+    hatch_mid_front(5, 14, 90), 
+    hatch_mid_back(5, -14, -90), 
+    hatch_high_front(33, 8.5, 90), 
+    hatch_high_back(33, -8.5, -90), 
+    cargo_pickup_front(2, 135, 113), 
+    cargo_pickup_back(2, -135, -113), 
+    cargo_shuttle_front(3, 29, 107), 
+    cargo_shuttle_back(3, -29, -107), 
+    cargo_rocketLow_front(21, 160, 72), 
+    cargo_rocketLow_back(21, -160, -72), 
+    cargo_rocketMid_front(9.5, 10, 83), 
+    cargo_rocketMid_back(9.5, -10, -83), 
+    cargo_rocketHigh_front(33, 1.5, 61), 
+    cargo_rocketHigh_back(33, -1.5, -61), 
+    manipulator_safe_minimum(36, 0, 0);
 
     private ManipulatorSetPoint(double elevatorHeight, double armAngle, double wristAngle)
     {
