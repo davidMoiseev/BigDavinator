5e953b1d5fa9b0fed864e280298e13a307b30d2a
olechiw <jakob.olechiw@gmail.com>
2019-04-01 19:09:29 -0400

    Auto initializer for easy auto choosing

--- src/main/java/frc/robot/auto/AutoRunner.java
+++ src/main/java/frc/robot/auto/AutoRunner.java
@@ -1,26 +1,43 @@
 package frc.robot.auto;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotCommandProvider;
 import frc.robot.auto.modes.AutoModeBase;
 import frc.robot.auto.modes.DriveStraightAuto;
+import frc.robot.auto.modes.FrontHatchAuto;
 
 public class AutoRunner
 {
     public static enum Auto
     {
-        DriveStraight
+        DriveStraight(() -> new DriveStraightAuto()),
+        FrontHatch(() -> new FrontHatchAuto());
+
+        public final Supplier<AutoModeBase> Initializer;
+        private Auto(Supplier<AutoModeBase> initializer)
+        {
+            this.Initializer = initializer;
+        }
     }
 
     AutoModeBase auton = null;
 
+    public static SendableChooser<Auto> BuildAutonChooser()
+    {
+        SendableChooser<Auto> autonChooser = new SendableChooser<>();
+        for (Auto auto : Auto.values())
+        {
+            autonChooser.addOption(auto.name(), auto);
+        }
+        return autonChooser;
+    }
+
     public void Select(Auto auto)
     {
-        switch (auto)
-        {
-        case DriveStraight:
-            auton = new DriveStraightAuto();
-            break;
-        }
+        auton = auto.Initializer.get();
     }
 
     public RobotCommandProvider Run()
