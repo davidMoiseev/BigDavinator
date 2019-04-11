23df82a9466db7932f86a43e2564d641e8a4fb1c
HOT Drive Laptop <hot.67.dev@gmail.com>
2019-04-10 20:44:08 -0400

    camera update, end day 1 states

--- src/main/java/frc/robot/VisionMotion.java
+++ src/main/java/frc/robot/VisionMotion.java
@@ -25,8 +25,8 @@ public class VisionMotion
     public static final double PLACE_AREA_FRONT = 5.36;
 
     //public static final double PICKUP_AREA_BACK = .857;
-    public static final double PICKUP_AREA_BACK = .791;
-    public static final double PICKUP_AREA_FRONT = 1.1;
+    public static final double PICKUP_AREA_BACK = .9;
+    public static final double PICKUP_AREA_FRONT = .9;
 
     Vision backCamera = new Vision("limelight-front", Vision.X_BACK, Vision.HEIGHT_BACK);
     Vision frontCamera = new Vision("limelight-back", Vision.X_FRONT, Vision.HEIGHT_FRONT);
